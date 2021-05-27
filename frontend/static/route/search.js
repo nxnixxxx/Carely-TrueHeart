8e3771a13d3071bb130b66e4474b511fb417f604
bxbeam <atittaya.jun@gmail.com>
2021-05-27 23:25:29 +0700

    เพิ่มไรไม่รู้ลืม

--- frontend/static/route/search.js
+++ frontend/static/route/search.js
@@ -23,7 +23,9 @@ router.post("/search", async(request, response) => {
     var reqBody = request.body;
     // [1] 2 dimensions array javascript
     // Query hotel
-    var idSim = {};
+    var arridSim = new Array();
+
+
     var count = 0;
     var sim = 0;
     var sum = 0;
@@ -31,41 +33,47 @@ router.post("/search", async(request, response) => {
 
 
     const query = Hotel.find({pet_type: request.body.pet_type}, (error, hotel) => {
-        if(error) console.log(error)
-        else{
-            hotel.forEach(function(h) {
-                hotelBody = Object.values(h)[5];
-                count = count + 1;
-                console.log(hotelBody);
-                // Start each hotel, Give sum = 0
-                sum = 0;
-                console.log("Hotel id: " + hotelBody['_id']);
-                Object.keys(hotelBody).forEach((key) => {
-                    if(key in reqBody){
-                        //console.log("reqBody value: " + reqBody[key] + " && hotelBody value: " + hotelBody[key]);
-                        if(reqBody[key] == hotelBody[key]){
-                            console.log(key + " is Match!! " + reqBody[key] + " " + hotelBody[key]);
-                            sum = sum +1;
-                        }
-                    }else{
-                        //console.log("This key is not in reqBody : " + key);
+        hotel.forEach(function(h) {
+            hotelBody = Object.values(h)[5];
+            count = count + 1;
+            //console.log(hotelBody);
+            // Start each hotel, Give sum = 0
+            sum = 0;
+            // console.log("Hotel id: " + hotelBody['_id']);
+            Object.keys(hotelBody).forEach((key) => {
+                if(key in reqBody){
+                    //console.log("reqBody value: " + reqBody[key] + " && hotelBody value: " + hotelBody[key]);
+                    if(reqBody[key] == hotelBody[key]){
+                        //console.log(key + " is Match!! " + reqBody[key] + " " + hotelBody[key]);
+                        sum = sum + 1;
                     }
-                }) 
-                //console.log("Sum Match: " + sum);
-                sim = sum/10;
-                //console.log("Sim: " + sim);
-                hotelId = hotelBody['_id'] ;
-                //console.log(hotelId);
-                idSim['key'+count] = [hotelId,sim];
-                // End loop key/value in Hotel
-                // [2] add value in 2 dimension array javascript
-                //console.log(idSim["key"+count]);
-               // idSim = idSim.sort((a, b) => a < b ? -1 : 1);
+                }else{
+                    //console.log("This key is not in reqBody : " + key);
+                }
+            }) 
+            //console.log("Sum Match: " + sum);
+            sim = sum/10;
+            //console.log("Sim: " + sim);
+            hotelId = hotelBody['_id'] ;
+            arridSim.push({
+                "hotelId" : hotelId,
+                "sim" : sim
             });
-        
-            console.log(idSim);
+        });
+        //Before Sort
+        console.log(arridSim);
 
-        }
+        //After Sort
+        arridSim.sort(function(a, b){
+            if(a.sim > b.sim){
+                return -1;
+            }
+            if(a.sim < b.sim){
+                return 1;
+            }
+            return 0;
+        });
+        console.log(arridSim);
     });
 
     response.sendFile(path.resolve("./", "frontend", "index.html"));
